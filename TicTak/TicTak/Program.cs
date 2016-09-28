dfa0203b2e0e3c25f9a5c252283d8bbb060bc31a
jasmi <jasmi@DESKTOP-T3PI036>
2016-09-28 13:48:11 -0400

    start

---
+++ TicTak/TicTak/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string p1 = "X";
+            string p2 = "O";
+
+            Console.WriteLine("Let's play Tic Tac Toe!");
+            Console.WriteLine();
+        
+            string[] grid = new string[9];
+
+
+
+            Board(grid);
+
+            Console.ReadLine();
+
+
+
+        }
+
+        string[] grid = new string[9];
+
+        static void Board(string[] table)
+        {
+            Console.WriteLine("{0}|{1}|{2}");
+            Console.WriteLine("-----------");
+            Console.WriteLine("{4}|{5}|{6}");
+            Console.WriteLine("-----------");
+            Console.WriteLine("{7}|{8}|{9}");
+        }
+        
+            
+
+
+
+
+
+    }
+}
