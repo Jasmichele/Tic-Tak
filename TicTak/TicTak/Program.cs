45bd4b8c27013db3f11ae907fda188f87070b654
jasmi <jasmi@DESKTOP-T3PI036>
2016-09-29 17:28:07 -0400

    Day2 Tik Tak

--- TicTak/TicTak/Program.cs
+++ TicTak/TicTak/Program.cs
@@ -12,38 +12,33 @@ namespace TicTak
         {
             string player1 = "X";
             string player2 = "O";
-
+            bool verify = true;
 
             Console.WriteLine("Let's play Tic Tac Toe!");
             Console.WriteLine();
             Console.WriteLine();
 
             string[] grid = new string[] { "0", "1", "2", "3", "4", "5", "6", "7", "8" };
-            
 
             Console.WriteLine("Player 1 is X");
             Console.WriteLine("Player 2 is O");
             Console.WriteLine();
             Console.WriteLine();
+            bool gameOver = false;
+            while (!gameOver)
+            {
+                Board(grid);
+                Cycle(grid);
+                Winner(grid);
+                Board(grid);
+                Cycle2(grid);
+                Winner(grid);
 
-         
-            Console.WriteLine("Player 1 pick spot");
-            Console.WriteLine();
-            Console.WriteLine();
-            Board(grid);
-            Cycle(grid);
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine("Player 2 pick spot");
-            Console.WriteLine();
-            Console.WriteLine();
-            Board(grid);
-            Cycle2(grid);
-          
-            
-           Console.ReadLine();
+
+            }
+            Console.ReadLine();
+            Console.Clear();
         }
-    
 
         static void Board(string[] table)
         {
@@ -52,10 +47,13 @@ namespace TicTak
             Console.WriteLine(string.Format("{0}|{1}|{2}", table[3], table[4], table[5]));
             Console.WriteLine("-----");
             Console.WriteLine(string.Format("{0}|{1}|{2}", table[6], table[7], table[8]));
+
         }
 
         static void Cycle(string[] p1)
         {
+            Console.WriteLine("Player 1 pick spot");
+
             int intAnswer;
             string answer = Console.ReadLine();
             intAnswer = Convert.ToInt32(answer);
@@ -64,13 +62,14 @@ namespace TicTak
                 Console.WriteLine("oops");
             }
             else
-                {
+            {
                 p1[intAnswer] = "X";
-                }
-        
+            }
         }
+
         static void Cycle2(string[] p2)
         {
+            Console.WriteLine("Player 2 pick spot");
             int intAnswer;
             string answer = Console.ReadLine();
             intAnswer = Convert.ToInt32(answer);
@@ -84,16 +83,56 @@ namespace TicTak
             }
 
         }
-        
-        
-         
-        
 
-         
+        static bool Winner(string[] array)
+        {
+            if (array[0] == array[1] && array[1] == array[2])
+            {
+                Console.WriteLine("Winner Winner!!");
+            }
+            else if (array[3] == array[4] && array[4] == array[5])
+            {
+                Console.WriteLine("Winner Winner!!");
+            }
+            else if (array[6] == array[7] && array[7] == array[8])
+            {
+                Console.WriteLine("Winner Winner!!");
+            }
+            else if (array[0] == array[3] && array[3] == array[6])
+            {
+                Console.WriteLine("Winner Winner!!");
+            }
+            else if (array[1] == array[4] && array[4] == array[7])
+            {
+                Console.WriteLine("Winner Winner!!");
+            }
+            else if (array[2] == array[5] && array[5] == array[8])
+            {
+                Console.WriteLine("Winner Winner!!");
+            }
+            else if (array[0] == array[4] && array[4] == array[8])
+            {
+                Console.WriteLine("Winner Winner!!");
+            }
+            else if (array[2] == array[4] && array[4] == array[6])
+            {
+                Console.WriteLine("Winner Winner!!");
+            }
+            return false;
+        }
 
-
-
-
+        static bool Tie(string[] array2)
+        {
+            for (int i = 0; i < array2.Length; i++)
+            {
+                if (array2[i] != "X" && array2[i] != "O")
+                {
+                    Console.WriteLine("Tie Game");
+            
+                }
+            }
+            return false;
+        }
 
     }
 }
