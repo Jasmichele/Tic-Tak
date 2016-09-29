4086428206295f78e339da025e76e18ed15abe4e
jasmi <jasmi@DESKTOP-T3PI036>
2016-09-29 09:00:01 -0400

    yesterday

--- TicTak/TicTak/Program.cs
+++ TicTak/TicTak/Program.cs
@@ -10,36 +10,86 @@ namespace TicTak
     {
         static void Main(string[] args)
         {
-            string p1 = "X";
-            string p2 = "O";
+            string player1 = "X";
+            string player2 = "O";
+
 
             Console.WriteLine("Let's play Tic Tac Toe!");
             Console.WriteLine();
-        
-            string[] grid = new string[9];
+            Console.WriteLine();
 
+            string[] grid = new string[] { "0", "1", "2", "3", "4", "5", "6", "7", "8" };
+            
 
+            Console.WriteLine("Player 1 is X");
+            Console.WriteLine("Player 2 is O");
+            Console.WriteLine();
+            Console.WriteLine();
 
+         
+            Console.WriteLine("Player 1 pick spot");
+            Console.WriteLine();
+            Console.WriteLine();
             Board(grid);
-
-            Console.ReadLine();
-
-
-
+            Cycle(grid);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Player 2 pick spot");
+            Console.WriteLine();
+            Console.WriteLine();
+            Board(grid);
+            Cycle2(grid);
+          
+            
+           Console.ReadLine();
         }
-
-        string[] grid = new string[9];
+    
 
         static void Board(string[] table)
         {
-            Console.WriteLine("{0}|{1}|{2}");
-            Console.WriteLine("-----------");
-            Console.WriteLine("{4}|{5}|{6}");
-            Console.WriteLine("-----------");
-            Console.WriteLine("{7}|{8}|{9}");
+            Console.WriteLine(string.Format("{0}|{1}|{2}", table[0], table[1], table[2]));
+            Console.WriteLine("-----");
+            Console.WriteLine(string.Format("{0}|{1}|{2}", table[3], table[4], table[5]));
+            Console.WriteLine("-----");
+            Console.WriteLine(string.Format("{0}|{1}|{2}", table[6], table[7], table[8]));
+        }
+
+        static void Cycle(string[] p1)
+        {
+            int intAnswer;
+            string answer = Console.ReadLine();
+            intAnswer = Convert.ToInt32(answer);
+            if (p1[intAnswer] == "X" || p1[intAnswer] == "O")
+            {
+                Console.WriteLine("oops");
+            }
+            else
+                {
+                p1[intAnswer] = "X";
+                }
+        
+        }
+        static void Cycle2(string[] p2)
+        {
+            int intAnswer;
+            string answer = Console.ReadLine();
+            intAnswer = Convert.ToInt32(answer);
+            if (p2[intAnswer] == "X" || p2[intAnswer] == "O")
+            {
+                Console.WriteLine("oops");
+            }
+            else
+            {
+                p2[intAnswer] = "O";
+            }
+
         }
         
-            
+        
+         
+        
+
+         
 
 
 
